87b4c649c3c00f1d26f552c2d5d86204a6701cb2
Dav Glass <davglass@gmail.com>
2013-04-23 08:56:30 -0500

    Updated version script to pull latest yui version, fixes #74

--- scripts/versions.js
+++ scripts/versions.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
 var path = require('path'),
+    http = require('http'),
     fs = require('fs'),
     package = require(path.join(__dirname, '../', 'package.json')),
     version = package.version,
-    doc = path.join(__dirname, '../', 'conf', 'docs', 'project.json');
+    doc = path.join(__dirname, '../', 'conf', 'docs', 'project.json'),
+    selleck = path.join(__dirname, '../', 'configs', 'selleck', 'project.json'),
+    yuidoc = path.join(__dirname, '../', 'configs', 'yuidoc', 'yuidoc.json');
 
 console.log('[version]', version);
 console.log('[doc]', doc);
@@ -14,4 +17,32 @@ var docJSON = require(doc);
 docJSON.version = version;
 
 fs.writeFileSync(doc, JSON.stringify(docJSON, null, 2));
-console.log('[done]');
+
+console.log('[fetch] latest YUI version');
+
+http.get({
+    hostname: 'yuilibrary.com',
+    path: '/api/v1/version'
+}, function(res) {
+    var data = '';
+
+    res.on('data', function(d) {
+        data += d;
+    });
+
+    res.on('end', function() {
+        var version = JSON.parse(data).version;
+        console.log('[yui]', version);
+        console.log('[selleck]', selleck);
+        var json = require(selleck);
+        json.yuiVersion = version;
+        fs.writeFileSync(selleck, JSON.stringify(json, null, 4) + '\n');
+
+        console.log('[yuidoc]', yuidoc);
+        var json = require(yuidoc);
+        json.version = version;
+        fs.writeFileSync(yuidoc, JSON.stringify(json, null, 4) + '\n');
+        console.log('[done]');
+    });
+});
+
